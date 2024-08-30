4bf7b6fe463787c6baa02baf9338952eeea513bd
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2024-08-30 19:27:23 -0300

    feat/criacao de componente de contador de caracteres

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SelectColorComponent } from './components/select-color/select-color.component';
+import { CharacterCounterComponent } from './components/character-counter/character-counter.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SelectColorComponent],
+  imports: [RouterOutlet, SelectColorComponent, CharacterCounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/character-counter/character-counter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './character-counter.component.html',
+  styleUrl: './character-counter.component.css'
+})
+export class CharacterCounterComponent {
+
+}
