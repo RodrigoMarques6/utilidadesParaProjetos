29ad4846029915176d305316f87e1c37e14f3af6
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2024-09-02 11:22:32 -0300

    feat/componente de contador de caracteres adicionado

--- src/app/components/character-counter/character-counter.component.ts
+++ src/app/components/character-counter/character-counter.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-character-counter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatFormFieldModule],
   templateUrl: './character-counter.component.html',
-  styleUrl: './character-counter.component.css'
+  styleUrl: './character-counter.component.css',
 })
 export class CharacterCounterComponent {
+  charCount: number = 0;
 
+  updateCharCount(event: Event): void {
+    const target = event.target as HTMLTextAreaElement;
+    this.charCount = target.value.length;
+  }
 }
