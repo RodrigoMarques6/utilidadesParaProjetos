baa0d71254279eab557ccc03cc92d55c7e91c19b
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2024-09-02 11:37:09 -0300

    feat/criando componentes de menu e footer para organizar o projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SelectColorComponent } from './components/select-color/select-color.component';
 import { CharacterCounterComponent } from './components/character-counter/character-counter.component';
-
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SelectColorComponent, CharacterCounterComponent],
+  imports: [
+    RouterOutlet,
+    SelectColorComponent,
+    CharacterCounterComponent,
+    FooterComponent,
+    MenuComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'utilidades';
