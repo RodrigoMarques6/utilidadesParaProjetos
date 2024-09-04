ee039543a787712212c17f8fb1605ac0b0ee470a
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2024-09-04 13:34:51 -0300

    feat/adicionando menu ao projeto

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, MatIconModule, MatMenuModule],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrl: './menu.component.css',
 })
-export class MenuComponent {
-
-}
+export class MenuComponent {}
