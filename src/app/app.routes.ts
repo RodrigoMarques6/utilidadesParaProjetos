11250b5bbac4e015d24e27f7b999f8231b367385
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2024-09-04 13:28:06 -0300

    feat/adição de novo layout na página inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { SelectColorComponent } from './components/select-color/select-color.component';
+import { CharacterCounterComponent } from './components/character-counter/character-counter.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'selecao-de-cor', component: SelectColorComponent },
+  { path: 'contador-de-caracteres', component: CharacterCounterComponent },
 ];
