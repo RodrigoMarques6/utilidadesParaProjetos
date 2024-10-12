d6e7a401f09fd1396e1804dd7fbb70c9ce096eb8
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2024-10-11 23:07:59 -0300

    feat/adicionando um novo componente de menu lateral

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { MainComponent } from './components/main/main.component';
 import { SelectColorComponent } from './components/select-color/select-color.component';
 import { CharacterCounterComponent } from './components/character-counter/character-counter.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
+import { SidebarRightComponent } from './components/sidebar-right/sidebar-right.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'selecao-de-cor', component: SelectColorComponent },
   { path: 'contador-de-caracteres', component: CharacterCounterComponent },
   { path: 'carrossel', component: CarouselComponent },
+  {path: 'menu-lateral-direito', component: SidebarRightComponent}
 ];
