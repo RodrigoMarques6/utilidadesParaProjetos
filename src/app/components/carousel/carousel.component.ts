084484cc0fc2c43b05f7af6c2380e07e57748e95
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2024-10-22 22:11:45 -0300

    feat/ajustes gerais no projeto

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import type { CarouselItem, CarouselInterface, CarouselOptions } from 'flowbite';
-import { initFlowbite, Carousel } from 'flowbite';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatInputModule } from '@angular/material/input';
@@ -14,32 +12,7 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './carousel.component.css',
 })
 export class CarouselComponent implements OnInit {
-  constructor(private el: ElementRef) {}
-  title = 'carrossel';
-  ngOnInit(): void {
-    initFlowbite();
+  constructor() {}
 
-    const items: CarouselItem[] = [
-      {
-        position: 0,
-        el: this.el.nativeElement.querySelector('#carousel-item-1'),
-      },
-      {
-        position: 1,
-        el: this.el.nativeElement.querySelector('#carousel-item-2'),
-      },
-      {
-        position: 2,
-        el: this.el.nativeElement.querySelector('#carousel-item-3'),
-      },
-      {
-        position: 3,
-        el: this.el.nativeElement.querySelector('#carousel-item-4'),
-      },
-    ];
-
-    const carousel: CarouselInterface = new Carousel();
-    // const carousel: CarouselInterface = new Carousel(items);
-    carousel.cycle();
-  }
+  ngOnInit(): void {}
 }
